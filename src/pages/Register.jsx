0eb8d2284db5334515d521ef93425053e98c1397
Abdullah Al Mamun <mamun12121999@gmail.com>
2023-06-03 17:09:03 +0600

    create functionality on register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -6,6 +6,10 @@ const Register = () => {
   const [fileError, setFileError] = useState("");
   const [image, setImage] = useState(null);
   const [fileDataURL, setFileDataURL] = useState(null);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   const handleFile = (e) => {
     const image = e.target.files[0];
@@ -39,77 +43,87 @@ const Register = () => {
     };
   }, [image]);
 
+  const handleChange = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <section className="bg-gray-100 min-h-screen flex flex-col">
       <div className="container max-w-lg mx-auto flex-1 flex flex-col items-center justify-center px-2">
-        <div className="bg-white px-6 py-8 rounded shadow-lg text-black w-full">
-          <h1 className="mb-8 text-3xl text-center">Sign up</h1>
-          <div className="flex items-center gap-x-2 mb-3">
-            <div className="h-[50px] w-[50px] rounded-full border">
-              {fileDataURL && (
-                <img
-                  className="rounded-full h-full w-full object-cover"
-                  src={fileDataURL}
-                  alt=""
+        <form onSubmit={handleChange}>
+          <div className="bg-white px-6 py-8 rounded shadow-lg text-black w-full">
+            <h1 className="mb-8 text-3xl text-center">Sign up</h1>
+            <div className="flex items-center gap-x-2 mb-3">
+              <div className="h-[50px] w-[50px] rounded-full border">
+                {fileDataURL && (
+                  <img
+                    className="rounded-full h-full w-full object-cover"
+                    src={fileDataURL}
+                    alt=""
+                  />
+                )}
+              </div>
+              <div>
+                <label htmlFor="image">Upload Image</label>
+                <input
+                  onChange={handleFile}
+                  className="hidden"
+                  type="file"
+                  accept=".png, .jpg, .jpeg"
+                  id="image"
                 />
-              )}
+              </div>
             </div>
-            <div>
-              <label htmlFor="image">Upload Image</label>
-              <input
-                onChange={handleFile}
-                className="hidden"
-                type="file"
-                accept=".png, .jpg, .jpeg"
-                id="image"
-              />
-            </div>
-          </div>
-          <input
-            type="text"
-            className="block border border-grey-light w-full p-3 rounded mb-4"
-            name="fullname"
-            placeholder="Full Name"
-          />
+            <input
+              onChange={(e) => setName(e.target.value)}
+              type="text"
+              className="block border border-grey-light w-full p-3 rounded mb-4"
+              name="fullname"
+              placeholder="Full Name"
+            />
 
-          <input
-            type="text"
-            className="block border border-grey-light w-full p-3 rounded mb-4"
-            name="email"
-            placeholder="Email"
-          />
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              type="text"
+              className="block border border-grey-light w-full p-3 rounded mb-4"
+              name="email"
+              placeholder="Email"
+            />
 
-          <input
-            type="password"
-            className="block border border-grey-light w-full p-3 rounded mb-4"
-            name="password"
-            placeholder="Password"
-          />
-          <input
-            type="password"
-            className="block border border-grey-light w-full p-3 rounded mb-4"
-            name="confirm_password"
-            placeholder="Confirm Password"
-          />
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              className="block border border-grey-light w-full p-3 rounded mb-4"
+              name="password"
+              placeholder="Password"
+            />
+            <input
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              type="password"
+              className="block border border-grey-light w-full p-3 rounded mb-4"
+              name="confirm_password"
+              placeholder="Confirm Password"
+            />
 
-          <button
-            type="submit"
-            className="w-full bg-[#38c172] text-center py-3 rounded bg-green text-white hover:bg-[#1f9d55] focus:outline-none my-1"
-          >
-            Create Account
-          </button>
-
-          <div className="text-grey-dark mt-6">
-            Already have an account?
-            <a
-              className="no-underline border-b border-blue text-blue"
-              href="../login/"
+            <button
+              type="submit"
+              className="w-full bg-[#38c172] text-center py-3 rounded bg-green text-white hover:bg-[#1f9d55] focus:outline-none my-1"
             >
-              Log in
-            </a>
-            .
+              Create Account
+            </button>
+
+            <div className="text-grey-dark mt-6">
+              Already have an account?
+              <a
+                className="no-underline border-b border-blue text-blue"
+                href="../login/"
+              >
+                Log in
+              </a>
+              .
+            </div>
           </div>
-        </div>
+        </form>
       </div>
     </section>
   );
